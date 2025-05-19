314abfaa01417df763721a8e4daf816016045879
Nuhtina <AlinkaN2005@mail.ru>
2025-05-19 08:02:58 +0300

    Добавьте файлы проекта.

---
+++ ApplicationData/AppConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static int AuthorID;
+        public static RecipeBookEntities3 model01;
+    }
+}
